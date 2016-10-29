c708354750003d2aefe654ff2032a075052c81cf
byronwasti <byron.wasti@gmail.com>
2016-10-29 15:29:16 -0400

    Solutions

--- day17/graph.py
+++ day17/graph.py
@@ -18,27 +18,47 @@ class Graph(object):
         return [head for head in self.nodes if node in self.successors(head)]
 
     def add_node(self, node):
-        raise NotImplementedError()
+        self.nodes.append(node)
+        self.adjacency_list[node] = []
 
     def add_edge(self, edge):
-        raise NotImplementedError()
+        self.adjacency_list[ edge[0] ].append(edge[1])
+
+    def print_self(self):
+        print(self.nodes)
+        print(self.adjacency_list)
 
 
 def bfs(graph, start):
     remaining_nodes = Queue()
     visited = set()
+    spanning = Graph([], [])
+    weights = {start: 0}
 
-    def visit(node):
+    def visit(node, distance):
         print(node)
         visited.add(node)
+        spanning.add_node(node)
         for tail in graph.successors(node):
             if tail not in visited:
                 remaining_nodes.put(tail)
+                spanning.add_edge( (node, tail) )
+                weights[tail] = distance + 1
+
+        return distance+1
 
     remaining_nodes.put(start)
+    distance = 0
     while not remaining_nodes.empty():
         n = remaining_nodes.get()
-        visit(n)
+        distance = visit(n, distance)
+
+    return (spanning, weights)
 
 g = Graph(['a', 'b', 'c', 'd', 'e'], [('a', 'b'), ('a', 'c'), ('b', 'd'), ('b', 'e'), ('e', 'a')])
-bfs(g, 'a')
+g.print_self()
+
+
+newGraph, weights = bfs(g, 'a')
+newGraph.print_self()
+print( weights )
